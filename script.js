8eebf00699f7dc2bd965067ca15de78e624628ae
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-06 11:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let finalResult="";
+for(let i=0;i<=100;i++){
+	if(i%3==0){
+		finalResult+="Fizz"+"\n";
+	}
+	else if(i%5==0){
+		finalResult+="Buzz"+"\n";
+	}
+	else if(i%3==0 && i%5==0){
+		finalResult+="FizzBuzz"+"\n";
+	}
+	else{
+		finalResult+=i+"\n";
+	}
+}
+alert(finalResult);
